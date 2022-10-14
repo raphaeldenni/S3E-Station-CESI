2f9984c1a3448aa6396418af75876eef1cf0487f
Raphaël Denni <65921801+SlyEyes@users.noreply.github.com>
2022-10-14 13:45:39 +0200

    Added sensor test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
+
 #define PINBTN 3
+
+#define PINLUM A0
+#define PINLUM1 A1
+
+#define PINTEMP A2
+#define PINTEMP1 A3
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(PINBTN, INPUT);
 
+  pinMode(PINLUM, OUTPUT);
+  pinMode(PINLUM1, OUTPUT);
+
+  pinMode(PINTEMP, OUTPUT);
+  pinMode(PINTEMP1, OUTPUT);
+
+  Serial.begin(9600);
+  /*
   if (digitalRead(PINBTN) == LOW) {
-    Serial.begin(9600);
     Serial.println("Button is pressed");
 
-  }
+  }*/
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("LUM : " + String(analogRead(PINLUM)));
+  Serial.println("LUM1 : " + String(analogRead(PINLUM1)));
+
+  delay(1000);
+
+  Serial.println("TEMP : " + String(analogRead(PINTEMP)));
+  Serial.println("TEMP1 : " + String(analogRead(PINTEMP1)));
+
+  delay(1000);
+
 }
