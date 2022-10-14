a41ac0a2fc65b0e4334dc7adf7e2dbdad35f7709
Pow3y <lukas.houille@viacesi.fr>
2022-10-14 13:41:06 +0200

    Test

--- src/main.c
+++ src/main.c
@@ -4,13 +4,10 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(PINBTN, INPUT);
-
   if (digitalRead(PINBTN) == LOW) {
     Serial.begin(9600);
     Serial.println("Button is pressed");
-
   }
-
 }
 
 void loop() {
