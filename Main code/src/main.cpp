b29234e1cfc77bfb0e58b6d1c89b27d11a5f28a4
Pow3y <lukas.houille@viacesi.fr>
2022-10-18 15:11:02 +0200

    added defines

--- Main code/src/main.cpp
+++ Main code/src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h> // Arduino library
-#include <SPI.h> // Serial Peripheral Interface library for communication with the SD card and GPS module
+#include <SPI.h>     // Serial Peripheral Interface library for communication with the SD card and GPS module
 #include <SD.h>      // Library for the SD card
 #include <SoftwareSerial.h> // Library for the GPS module
 
-#include <ChainableLED.h> // Library for the LED strip
+#include <ChainableLED.h>    // Library for the LED strip
 #include <Adafruit_BME280.h> // Library for the BME280 sensor
 #include <TinyGPSPlus.h>     // Library for the GPS module
 
-#define GBTN_PIN 2 // initialize the pin for the green button
-#define RBTN_PIN 3 // initialize the pin for the red button
+#define GBTN_PIN 2 // define the pin for the green button
+#define RBTN_PIN 3 // define the pin for the red button
 
-#define LED_PIN 5   // initialize the pin for the LED
+#define LED_PIN 5   // define the pin for the LED
 #define LED_DATA_PIN 6  // initialize the pin for the LED
 #define LEDS_NUM 1 // number of LEDs in the chain
 
@@ -20,10 +20,21 @@
 #define TEMP_PIN A2  // first temperature sensor pin
 #define TEMP_PIN1 A3 // second temperature sensor pin
 
-//#define SDPIN 4 // initialize the pin for the SD card
+#define SDPIN 4 // define the pin for the SD card
 
-#define GPS_RX 7 // initialize the pin for the GPS module
-#define GPS_TX 8 // initialize the pin for the GPS module
+#define GPS_RX 7 // define the pin for the GPS module
+#define GPS_TX 8 // define the pin for the GPS module
+
+#define STANDARD 1 // define the value of the standard mode
+#define CONFIGURATION 2  // define the value of the configuration mode
+#define ECONOMY 3  // define the value of the economy mode
+#define MAINTENANCE 4  // define the value of the maintenance mode
+#define ERROR_CLOCK_ACCESS 5  // define the value of the clock error
+#define ERROR_GPS 6  // define the value of the GPS error mode
+#define ERROR_CAPTOR_ACCESS 7  // define the value of the captor acess error mode
+#define ERROR_DATA_INCOHERENCE 8  // define the value of the INCOHERENCE error mode
+#define ERROR_SD_FULL 9  // define the value of the SD card FULL error mode
+#define ERROR_SD_WRITE 10  // define the value of the BME280 access error mode
 
 ChainableLED leds(LED_PIN, LED_DATA_PIN, LEDS_NUM);
 
